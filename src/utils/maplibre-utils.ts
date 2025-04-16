ac16681340379fccdba495997aac6a9d68b39877
Wenqi He <galmungral@gmail.com>
2025-04-16 12:53:42 -0500

    fix: remove import from src

--- src/utils/maplibre-utils.ts
+++ src/utils/maplibre-utils.ts
@@ -35,5 +35,5 @@ export function getMetersPerPixelAtLatitude(lat: number, zoom: number): number {
 export const ABORT_ERROR = 'AbortError';
 
 export function isAbortError(error: Error): boolean {
-    return error.message === ABORT_ERROR;
+  return error.message === ABORT_ERROR;
 }
