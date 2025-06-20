76f7cafbb9bebdb0a210e5fdd11a4c19365a3f1a
Wenqi He <galmungral@gmail.com>
2025-06-20 12:17:35 -0500

    feat: add custom redux hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore } from '@reduxjs/toolkit';
+import { createContext } from 'react';
+import {
+  ReactReduxContextValue,
+  createDispatchHook,
+  createSelectorHook,
+  createStoreHook,
+} from 'react-redux';
 
 import { exploreSlice } from '@ncsa/geo-explorer/store/explore/slice';
 
@@ -11,6 +18,15 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
+export const GeoExplorerReduxContext = createContext<ReactReduxContextValue<
+  unknown,
+  Action
+> | null>(null);
+
+export const useStore = createStoreHook(GeoExplorerReduxContext);
+export const useSelector = createSelectorHook(GeoExplorerReduxContext);
+export const useDispatch = createDispatchHook(GeoExplorerReduxContext);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
